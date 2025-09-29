58064d027f7476dd6d0cc478dc80d182f848fb66
hassaan024 <hassaanbukhari24@gmail.com>
2025-09-28 21:46:50 -0500

    ap configuration

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hotspots</Text>
+      <Text style={styles.subtitle}>See what’s trending around you</Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#fff" },
+  title: { fontSize: 28, fontWeight: "bold", color: "#003087", marginBottom: 8 },
+  subtitle: { fontSize: 16, color: "#555", marginBottom: 20 },
+  button: { backgroundColor: "#dba734", paddingHorizontal: 20, paddingVertical: 12, borderRadius: 8 },
+  buttonText: { color: "#000", fontWeight: "600" }
 });
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# CSC 403 - Floor
-# Team: Journey, Fariza, Dylan, Hassaan
+# Hotspots (CSC403)
+## Team: Journey, Fariza, Dylan, Hassaan
+
+### About the app
+Hotspots is a social app built with **React Native + Expo** with an aim to show the user what is trending around them using an interactive heatmap.  
+
+### Build Instructions
+
+#### Clone the repo and install dependencies:
+
+```bash
+git clone <your-ssh-url> Hotspots
+cd Hotspots
+npm install
+```
+
+#### Run the app
+
+```bash
+npx expo start
+```
+If emulator is open press 'a' on the terminal to open in the emulator. You can also open it on localhost with the link given in the terminal and should be able to see the app in the browser. 
+
+#### How to package into an app on the emulator
+
+Add a new file to android folder 'local.properties' and add path to your sdk files. This path can be found in Android Studio -> SDK Manager. Insert the path
+
+sdk.dir={Your SDK path}
+
+Now run the following commands
+
+```bash
+npx expo prebuild
+npx expo run:android
+```
+
+After successfully running them, an app should appear on the emulator running
+
