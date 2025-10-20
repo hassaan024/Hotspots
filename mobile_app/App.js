30ed0c19d22ea06e2a09fb5276e50019e73a5515
hassaan024 <hassaanbukhari24@gmail.com>
2025-10-19 22:16:12 -0500

    Added Profile page

--- mobile_app/App.js
+++ mobile_app/App.js
@@ -7,12 +7,13 @@ import Navbar from "./components/Navbar";
 import MapPage from "./pages/MapPage";
 import PostsPage from "./pages/PostsPage";
 import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage"; // <-- Import ProfilePage
 
 import { styles } from "./styles";
 import { AuthContext } from "./AuthContext";
 
 export default function App() {
-  const [page, setPage] = React.useState("posts"); // "map" | "posts"
+  const [page, setPage] = React.useState("posts"); // "map" | "posts" | "profile"
   const [user, setUser] = React.useState(null);
 
   const REQUIRE_LOGIN = Constants.expoConfig?.extra?.REQUIRE_LOGIN ?? true;
@@ -45,6 +46,7 @@ export default function App() {
             <View style={styles.content}>
               {page === "map" && <MapPage />}
               {page === "posts" && <PostsPage />}
+              {page === "profile" && <ProfilePage />}
             </View>
             <Navbar current={page} onChange={setPage} />
           </>
